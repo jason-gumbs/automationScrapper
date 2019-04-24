551143beab6b4cb9adbe07935ddf400e70a7a23c
wxg8ihv <wilfred_gumbs@homedepot.com>
2019-04-23 22:26:22 -0400

    working app

---
+++ client/build/precache-manifest.8335890e47a2158a97f0e15f2284a0e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "196d8fa6a2be19deb996",
+    "url": "/static/css/main.cc7c0d78.chunk.css"
+  },
+  {
+    "revision": "196d8fa6a2be19deb996",
+    "url": "/static/js/main.3ea85afe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9b965979f7cd115a9629",
+    "url": "/static/css/2.2148e321.chunk.css"
+  },
+  {
+    "revision": "9b965979f7cd115a9629",
+    "url": "/static/js/2.f4fabab8.chunk.js"
+  },
+  {
+    "revision": "5c8fc8a41313d442a243001a567ad130",
+    "url": "/index.html"
+  }
+];
